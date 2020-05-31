dc98745d7e36c1b2095ac1224305f3582ed01a93
Yuanyuan <yuanyuandora@gmail.com>
2020-05-31 17:46:16 +1000

    [hotfix] Cosmetic updates and remove unused component

--- src/fancy-button/fancy-button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './fancy-button.scss'
-
-const FancyButton = ({label}) => (
-    <button className={styles.button}>{label}</button>
-);
-
-export default FancyButton;
