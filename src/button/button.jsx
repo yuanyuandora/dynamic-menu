87017d67b86d4718f632675bb071884dc5e829d6
Yuanyuan <yuanyuandora@gmail.com>
2020-05-31 16:44:50 +1000

    Add unit test for the root app component

--- src/button/button.jsx
+++ src/button/button.jsx
@@ -5,27 +5,45 @@ class Button extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            subMenu: [],
-            openSubMenu: false
+            buttonStyle: styles.button
         };
         this.handleClick = this.handleClick.bind(this);
+    }
+
+    componentDidMount() {
+        switch (this.props.buttonProps.type) {
+            case "form":
+                this.setState({
+                    buttonStyle: this.state.buttonStyle + (" active-button form-button")
+                });
+                break;
+            case "nested":
+                this.setState({
+                    buttonStyle: this.state.buttonStyle + (" active-button nested-button")
+                });
+                break;
+            case "placeholder":
+                this.setState({
+                    buttonStyle: this.state.buttonStyle + (" inactive-button")
+                });
+                break;
+        }
     }
 
     handleClick() {
         switch (this.props.buttonProps.type) {
             case "form":
                 this.props.showForm(this.props.buttonProps.formFields, this.props.parentMenuData);
+
                 break;
             case "nested":
                 if (this.props.buttonProps.children && this.props.buttonProps.children.length > 0) {
-                    // this.setState({
-                    //     subMenu: this.showSubMenu(this.props.buttonProps.children)
-                    // });
                     this.props.showMenu(this.props.buttonProps.children);
                 }
                 break;
             case "placeholder":
                 this.props.showMessage("Coming Soon...");
+
                 break;
         }
 
@@ -33,7 +51,7 @@ class Button extends React.Component {
 
     render() {
         return (
-            <button className={styles.button} onClick={this.handleClick}>{this.props.buttonProps.name}</button>
+            <button className={this.state.buttonStyle} onClick={this.handleClick}>{this.props.buttonProps.name}</button>
         );
     }
 }
